4e505ce033db3cfe4761ace5cd6c5b96ba87356a
Callum <callum@sidekickhealth.com>
2023-03-18 20:51:13 +0000

    Updated text on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  // Your JavaScript code goes here
+</script>
+
+<style>
+  /* Your CSS styles go here */
+  h1 {
+    font-size: 2.5rem;
+    font-weight: bold;
+    text-align: center;
+  }
+
+  p {
+    font-size: 1.25rem;
+    text-align: center;
+  }
+</style>
+
+<!-- Your HTML markup goes here -->
+<main>
+  <h1>Welcome to Overstat</h1>
+  <p>Analyze your Overwatch 2 team compositions and improve your game.</p>
+</main>
